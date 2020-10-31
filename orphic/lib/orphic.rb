23fa91c67ceac0a8d0e854a425e3d52ae116dad1
Kyle OBrien <obrienk@webbhost.net>
2020-10-31 05:24:55 -0500

    Rename main class to Mjolnir, add hn subclass

--- orphic/lib/orphic.rb
+++ orphic/lib/orphic.rb
@@ -2,5 +2,8 @@ require "orphic/version"
 require "orphic/cli"
 
 module Orphic
-  # my code here...
+  puts "Startled, you wake in a circular room surrounded by bright terminals."
+  puts "Each terminal represents a pathway that is locked behind a series of keys."
+  puts "Combining the skills gained from each path is possible, but wild paths taken will be less worn."
+  puts "Some of these skills overlap. Learn wisely."
 end
