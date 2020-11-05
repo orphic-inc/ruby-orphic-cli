e145231ec2ace6e34ac5cef7279dddb04cc30dbd
Kyle OBrien <obrienk@webbhost.net>
2020-11-05 03:47:00 -0600

    Add cli-ui gem dependency, start adding UI support

--- orphic/lib/orphic/cli.rb
+++ orphic/lib/orphic/cli.rb
@@ -1,9 +1,15 @@
 require 'thor'
 require 'orphic/cli/node'
 require 'orphic/cli/district'
+require 'cli/ui'
 
 module Orphic
   class Mjolnir < Thor
+    CLI::UI::StdoutRouter.enable
+    CLI::UI::Frame.open('Orphic Developer Experience') do
+      CLI::UI::Frame.open('Ruby CLI') { puts "The Orphic Ruby CLI utilizes the Thor gem for self-documenting commands and cli-ui gem for UI." }
+      puts "Great minds never think alike."
+    end
     desc "hello NAME", "This will greet you"
     long_desc <<-HELLO_WORLD
 
