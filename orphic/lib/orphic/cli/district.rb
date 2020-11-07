c7c345cd8a818d54195a2a834e89097a730400c6
Kyle OBrien <obrienk@webbhost.net>
2020-11-06 21:48:35 -0600

    capitalize option params for visual clarity

--- orphic/lib/orphic/cli/district.rb
+++ orphic/lib/orphic/cli/district.rb
@@ -17,18 +17,18 @@ module Orphic
       long_desc <<-CREATE_DISTRICT
         Create a new district with createDistrict. Base map includes the following options for branching:
 
-        --awake - The entrypoint. Create command line interfaces, web portals, and related tech.
-        --grow - User Interfaces and Experiences, projects here relate to front-end like React, CSS, and prototypes.
-        --discover - Your current location. Educate your peers or yourself.
-        --trust - Create authorization, cyphers, and cryptography-related projects.
-        --truth - The gateway. Can be used to verify authorizations and is often tied to the Trust District.
-        --magic - Let the magic begin! Here you can create ephemeral changes and middleware.
-        --poetry - The poet, the lover, and the coder are of imagination all compact... Code as art.
-        --drive - Real-time commands that will effect a concurrent stream or processes.
-        --will - Databases and permanence.
-        --loyal - The gauntlet. Test your loyalty with user or unit testing.
-        --enthuse - Live events and promotions.
-        --clarity - The endgame, from your command-line, through staging and production.
+        --AWAKE - The entrypoint. Create command line interfaces, web portals, and related tech.
+        --GROW - User Interfaces and Experiences, projects here relate to front-end like React, CSS, and prototypes.
+        --DISCOVER - Your current location. Educate your peers or yourself.
+        --TRUST - Create authorization, cyphers, and cryptography-related projects.
+        --TRUTH - The gateway. Can be used to verify authorizations and is often tied to the Trust District.
+        --MAGIC - Let the magic begin! Here you can create ephemeral changes and middleware.
+        --POETRY - The poet, the lover, and the coder are of imagination all compact... Code as art.
+        --DRIVE - Real-time commands that will effect a concurrent stream or processes.
+        --WILL - Databases and permanence.
+        --LOYAL - The gauntlet. Test your loyalty with user or unit testing.
+        --ENTHUSE - Live events and promotions.
+        --CLARITY - The endgame, from your command-line, through staging and production.
       CREATE_DISTRICT
       option :awake
       option :grow
