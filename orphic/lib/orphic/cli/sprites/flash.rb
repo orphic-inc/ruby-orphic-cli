a3f5f7997d0703e8dd9a55a0d5325371c0f9316a
Kyle OBrien <obrienk@webbhost.net>
2020-12-28 18:38:44 -0600

    Flash and Geek sprites functional-ish

--- orphic/lib/orphic/cli/sprites/flash.rb
+++ orphic/lib/orphic/cli/sprites/flash.rb
@@ -1,2 +1,6 @@
 # todo
+require 'orphic/cli/sprites/geek'
+
 # send discs here, this is where the lifecycle of a composition
+puts {"hi from Flash"}
+
--- orphic/lib/orphic/cli/sprites/geek.rb
+++ orphic/lib/orphic/cli/sprites/geek.rb
@@ -1,9 +1,21 @@
 # pseudo-code
-data = {
+class Data
+  
   # required datasets
-  i = ins; # instructions, bootload 'this'
-  d = disc; # check if additional instructions available, if not disconnect
+  def i(ins)
+    # if (Cursor <= i) then
+    puts  "i !<= 0"
+    $ins = "ins"
+    return $ins
+  end
+  # instructions, bootload 'this'
+  def d(disc)
+    $d = "disc"
+    puts $d
+    puts "disc"
+  end
+  # check if additional instructions available, if not disconnect
   # not required but recommended for when things break
-  a = audio;
-  v = video;
-}
+  $a = __FILE__
+  $v = __LINE__
+end
